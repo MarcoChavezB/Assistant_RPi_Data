97a426f0a9e2313977af2da96ba13f883c20fb88
Alonso <abrahamreynoso64@gmail.com>
2024-03-11 07:58:58 -0600

    Props

--- Clases/SensorGPS.py
+++ Clases/SensorGPS.py
@@ -1,3 +1,6 @@
 class SensorGPS:
-    def __init__(self):
-        pass
+    def __init__(self, id, latitud, longitud, date):
+        self.id = id
+        self.latitud = latitud
+        self.longitud = longitud
+        self.date = date
