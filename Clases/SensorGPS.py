8378245bde9003a7c932373b434aca8fe7b884d1
anthony <tony20402014@gmail.com>
2024-03-10 21:42:24 -0600

    string

--- Clases/SensorGPS.py
+++ Clases/SensorGPS.py
@@ -4,3 +4,14 @@ class SensorGPS(Controlador):
       super().__init__()
       self.date = date
       self.valor = valor
+
+
+  def __str__(self):
+        if not self.informacion:
+            return (
+                f"\ndate: {self.date}\n"
+                f"valor: {self.valor}\n"
+            )
+        else:
+            elementos_str = [str(elemento) for elemento in self.informacion]
+            return "\n".join(elementos_str)
