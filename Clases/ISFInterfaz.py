d90cb2ae1ef7fc720601ebd47c531602a738a135
VictoriaReyes <reyedvictoria1803@gmail.com>
2024-03-22 11:56:48 -0600

    clases base

---
+++ Clases/ISFInterfaz.py
@@ -0,0 +1,11 @@
+from ISF import ISF
+class ISFInterfaz():
+    def __init__(self, ISF =None ):
+        if ISF is None:
+                self.ISFisico = ISF()
+                self.isConected = True 
+                self.ISFisico.cargar_desde_archivo("InfoSensorFisico.json")
+        else:
+                self.ISFisico = ISF()
+                self.isConected = False
+           
