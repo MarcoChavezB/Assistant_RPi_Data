29eb74155bef9cb6e4b15c2645f4765ca9daf6b7
VictoriaReyes <reyedvictoria1803@gmail.com>
2024-04-06 19:28:42 -0600

    sensores json

--- Clases/Carrito.py
+++ Clases/Carrito.py
@@ -6,49 +6,54 @@ import requests
 from codeService import codeService
 
 class Carrito():
-    def __init__(self,clave=None, nombre=None, descripcion=None):
+    def __init__(self,nombre=None ,clave=None, type=None, model=None):
         super().__init__()
-        self.isConected= clave==None and nombre==None and descripcion==None
+        self.isConected= nombre = nombre and clave==None and type==None and model==None
         if self.isConected:
             self.arreglo=[]
-
-        self.arreglo=None
-
+        self.nombre=nombre
         self.clave=clave
-        self.nombre = nombre
-        self.descripcion = descripcion
+        self.type = "Car"
+        self.model = "Assistant"
         self.codeserv = codeService()
 
     def gen_code(self):
-        with open('json/UniqueCode.json') as json_file:
+        with open('Clases/json/UniqueCode.json') as json_file:
             data = json.load(json_file)
             deviceCode = data['deviceCode']
 
         while True:
             if deviceCode is None or deviceCode == "":
                 chars = string.ascii_letters + string.digits
-                code = ''.join(random.choice(chars) for _ in range(10))
-                response = self.api_request(code)
+                code = ''.join(random.choice(chars) for _ in range(10))     
+                response = self.api_request_device(code,self.type ,self.model)
                 if response is not None:
                     data['deviceCode'] = code
-                    with open('json/UniqueCode.json', 'w') as json_file:
+                    with open('Clases/json/UniqueCode.json', 'w') as json_file:
                         json.dump(data, json_file)
                     break
             else:
                 print("Car already exists")
                 break
 
-    def api_request(self, code):
+    def device_code(self):
+        with open('Clases/json/UniqueCode.json') as json_file:
+            data = json.load(json_file)
+            self.clave = data['deviceCode']
+            return data['deviceCode']
 
-        api_url = "http://backend.mylittleasistant.online:8000/api/store/device"
+
+    def api_request_device(self, code, type,model):
+
+        api_url = "http://backend.mylittleasistant.online:8000/api/device/store"
 
         body = {
             "code": code,
-            "type": "Car",
-            "model": "Assistant",
+            "type": type,
+            "model": model,
             "os": platform.system(),
         }
-
+    
         try:
             response = requests.post(api_url, headers=self.codeserv.get_headers(), json=body)
             response.raise_for_status()
@@ -63,6 +68,11 @@ class Carrito():
 if __name__ == "__main__":
     carrito = Carrito()
     carrito.gen_code()
+    print(carrito.device_code())
+    print(carrito.nombre)
+    print(carrito.type)
+    print(carrito.model)
+    
 
         
     
