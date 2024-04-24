d23de85ba6fa498683a6fd60c7e954f9eb765b51
VictoriaReyes <reyedvictoria1803@gmail.com>
2024-04-24 10:49:44 -0600

    ya

--- Clases/main.py
+++ Clases/main.py
@@ -1,13 +1,12 @@
 from Controlador import Controlador
 from Carrito import Carrito
 from Data import Data
-from Datagps import Datagps
 
 
 controlador = Controlador()
 carrito = Carrito()
 data = Data()
-gps = Datagps()
+
 carrito.gen_code()
 data.enviar_sensor()
     
